d134d3dceba6e6997918518fdf5dd7c3e59ea3bc
tuzzer <matthew.tk.chan@gmail.com>
2014-10-10 17:21:56 -0400

    Added function that calculate the expected reward removed Regions Manager altogether

--- Software/CBLA/CBLA_Behaviour.py
+++ Software/CBLA/CBLA_Behaviour.py
@@ -1,4 +1,42 @@
 
+import math
+from RegionsManager import Expert
 
 if __name__ == "__main__":
-    print(1+2)
+
+    # generating exemplars
+    exemplars = []
+    for i in range(1,15):
+        exemplar = ((math.floor(100*math.sin(math.pi*i/4)),),
+                    (math.floor(100*math.sin(math.pi*i/3)),),
+                    (math.floor(100*math.sin(math.pi*i/2)),))
+        exemplars.append(exemplar)
+    print("Generated exemplars: ", exemplars)
+
+    # instantiate an Expert
+    expert = Expert()
+
+    # appending data to expert
+    for exemplar in exemplars:
+
+        S = exemplar[0]
+        M = exemplar[1]
+        S1 = exemplar[2]
+        print("\n Test case ", S, M, S1)
+
+        # have the expert make prediction
+        S1_predicted = expert.predict(S, M)
+        print(S1_predicted)
+
+        # do action
+
+        # add exemplar to expert
+        expert.append(S + M, S1)
+        expert.split()  # won't actually split if the condition is not met
+
+        L, M1 = expert.get_expected_reward(S1)
+        print("Expected Reward", L)
+        print("Next Action", M1)
+
+    expert.print()
+
--- Software/CBLA/RegionsManager.py
+++ Software/CBLA/RegionsManager.py
@@ -1,30 +1,6 @@
 __author__ = 'Matthew'
 
-from collections import defaultdict
-from sklearn import tree
 import math
-import itertools
-
-class RegionsManager():
-
-    def __init__(self, S0, M0, S1):
-
-        if not isinstance(S0, tuple):
-            raise(TypeError, "S0 must be a tuple")
-        if not isinstance(M0, tuple):
-            raise(TypeError, "M0 must be a tuple")
-        if not isinstance(S1, tuple):
-            raise(TypeError, "S1 must be a tuple")
-
-        # memory
-        self.S0 = S0
-        self.M0 = M0
-
-        # the root expert
-        self.expert = Expert()
-        self.expert.append(S0 + M0, S1)
-
-
 
 class Expert():
 
@@ -48,6 +24,9 @@ class Expert():
         # knowledge gain assessor
         self.kga = KGA(self.mean_error)
 
+        # histroical reward history
+        self.rewards_history = [0]
+
     def append(self, SM, S1):
 
         if not isinstance(SM, tuple):
@@ -66,11 +45,12 @@ class Expert():
             # update the KGA
             self.kga.append_error(SM[0:len(S1)], S1)
             self.mean_error = self.kga.calc_mean_error()  # used to determine if splitting is necessary
+            self.rewards_history.append(self.kga.calc_reward())
 
         # TODO: add cases when only one of the child is NONE
 
         # delegate to child nodes
-        elif self.region_splitter.classify(S+M):
+        elif self.region_splitter.classify(SM):
             self.right.append(SM, S1)
         else:
             self.left.append(SM, S1)
@@ -87,6 +67,7 @@ class Expert():
 
         # this is leaf node
         if self.left is None and self.right is None:
+            # Todo: need a real regressor
             return S
 
         # TODO: add cases when only one of the child is NONE
@@ -127,6 +108,15 @@ class Expert():
                 self.training_data = []
                 self.training_label = []
 
+                # clear everything as they are not needed any more
+                self.mean_error = None
+
+                # knowledge gain assessor
+                self.kga = None
+
+                # histroical reward history
+                self.rewards_history = None
+
         else:
             # delegate to child nodes
             self.right.split()
@@ -141,6 +131,7 @@ class Expert():
 
         # this is leaf node
         if self.left is None and self.right is None:
+            # TODO: deal cases when there's no training data
 
             # find out the indices of M data
             M_index = (len(self.training_data[0]) - len(S1), len(self.training_data[0]))
@@ -152,19 +143,28 @@ class Expert():
             # take the average of the M1 in each dimension
             M1 = tuple([sum(M1[i])/len(M1[i]) for i in range(len(M1))])
 
-            expected_reward = self.kga.calc_expected_reward()
+            if self.is_possible(S1, M1):
+                # reward is just the reward in the most recent time region
+                expected_reward = self.rewards_history[-1]
+            else:
+                expected_reward = -9999999999.999
 
             return (expected_reward, M1)
-        #
-        # # TODO: add cases when only one of the child is NONE
-        #
-        # # delegate to child nodes
-        # if self.region_splitter.classify(S+M):
-        #     return self.right.predict(S,M)
-        # else:
-        #     return self.left.predict(S,M)
 
+        # TODO: add cases when only one of the child is NONE
 
+        # return the child node with the largest reward
+        expected_reward_L = self.left.get_expected_reward(S1)
+        expected_reward_R = self.right.get_expected_reward(S1)
+
+        if expected_reward_L[0] > expected_reward_R[0]:
+            return expected_reward_L
+        else:
+            return expected_reward_R
+
+    def is_possible(self, S1, M1):
+        # Todo: figure out how to tell if an action is possible given a state
+        return True
 
     def print(self, level=0):
 
@@ -181,8 +181,27 @@ class Expert():
 class RegionSplitter():
 
     def __init__(self, data, label):
-        self.cut_dim = 1
-        self.cut_val = 80
+
+        self.cut_dim = 0
+        self.cut_val = 0
+
+        # Todo: need proper clustering
+
+        sample_num = len(data)
+        dim_num = len(data[0])
+
+        # combina the data and the label for sorting purposes
+        exemplars = [(data[i], label[i]) for i in range(sample_num)]
+
+        # sort in each dimension
+        dim_max_range = 0
+        for i in range(dim_num):
+            exemplars.sort(key=lambda exemplar: exemplar[0][i])
+
+            if dim_max_range < exemplars[-1][0][i] - exemplars[0][0][i]:
+                self.cut_dim = i
+
+        self.cut_val = exemplars[int(sample_num/2)][0][self.cut_dim]
 
     def classify(self, data):
         if not isinstance(data, tuple):
@@ -219,42 +238,42 @@ class KGA():
     def calc_mean_error(self):
 
         mean_error = math.fsum(self.errors[-self.delta:])/self.delta
-        print(mean_error)
         return mean_error
 
     def metaM(self):
 
         mean_error_predicted = math.fsum(self.errors[-(self.delta+self.tau):-(self.tau)])/self.delta
-        print(mean_error_predicted)
         return mean_error_predicted
 
-    def calc_expected_reward(self):
+    def calc_reward(self):
 
-        return self.calc_mean_error() - self.metaM()
+        return self.metaM() - self.calc_mean_error()
 
 if __name__ == "__main__":
 
-    memory_data = []
-    for i in range(1,2):
+    # generating exemplars
+    exemplars = []
+    for i in range(1,15):
         exemplar = ((math.floor(100*math.sin(math.pi*i/4)),),
                     (math.floor(100*math.sin(math.pi*i/3)),),
                     (math.floor(100*math.sin(math.pi*i/2)),))
-        memory_data.append(exemplar)
+        exemplars.append(exemplar)
 
-    print(memory_data)
+    print("Generated exemplars", exemplars)
 
-    RM = RegionsManager(memory_data[0][0], memory_data[0][1], memory_data[0][1])
-    for data in memory_data:
-        RM.expert.append(data[0] + data[1], data[1])
-        RM.expert.split()
+    # instantiate an Expert
+    expert = Expert()
+    for data in exemplars:
+        expert.append(data[0] + data[1], data[1])
+        expert.split()
 
     S = (70,)
     M = (90,)
-    print(RM.expert.predict(S, M))
+    print(expert.predict(S, M))
     S1 = (40,)
-    RM.expert.append(S+M, S1)
-    RM.expert.split()
-    RM.expert.print()
+    expert.append(S+M, S1)
+    expert.split()
+    expert.print()
 
-    print(RM.expert.get_expected_reward(S1))
-    print(RM.expert.kga.errors)
+    print("Expected Reward", expert.get_expected_reward(S1))
+    print("Errors", expert.kga.errors)
