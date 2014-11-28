6a76499ca12d5734169532d68eef49ece1768409
tuzzer <matthew.tk.chan@gmail.com>
2014-11-28 18:25:01 -0500

    added plane funciton and some other simple functions

--- Software/CBLA/SimSystem.py
+++ Software/CBLA/SimSystem.py
@@ -87,11 +87,75 @@ class SimpleFunction2(SimpleFunction):
         else:
             for i in range(len(self.S)):
                 for m in M:
-                    S1[i] += math.sin(m/25.0)*10.0 + random.uniform(M[0]/25, -M[0]/25)
+                    S1[i] += math.sin(m/7.0)*10.0 + random.uniform(M[0]/15, -M[0]/15)
 
         self.S = tuple(S1)
         self.M0 = M
 
+
+class SimpleFunction3(SimpleFunction):
+
+    def actuate(self, M):
+        if not isinstance(M, tuple):
+            raise(TypeError, "M must be a tuple")
+        if len(M) != len(self.M0):
+            raise(ValueError, "M must be %d dimension" % len(self.M0))
+
+        S1 = [0]*len(self.S)
+
+
+        for i in range(len(self.S)):
+            for m in M:
+                if m >25:
+                    S1[i] += M[0]/5
+                elif m > -15:
+                    S1[i] += math.sin(m/15.0)*10.0
+                else:
+                    S1[i] += math.sin(m/15.0)*10.0 + random.uniform(m/25, -m/25)
+
+
+        self.S = tuple(S1)
+        self.M0 = M
+
+class DiagonalPlane(SimpleFunction):
+    def actuate(self, M):
+        if not isinstance(M, tuple):
+            raise(TypeError, "M must be a tuple")
+        if len(M) != len(self.M0):
+            raise(ValueError, "M must be %d dimension" % len(self.M0))
+
+        S1 = [0]*len(self.S)
+
+
+        for i in range(len(self.S)):
+            if math.fsum(M) > 0:
+                S1[i] = 0
+            else:
+                S1[i] =0 + random.uniform(-60, 60)
+
+        self.S = tuple(S1)
+        self.M0 = M
+
+class DiagonalPlane2(SimpleFunction):
+    def actuate(self, M):
+        if not isinstance(M, tuple):
+            raise(TypeError, "M must be a tuple")
+        if len(M) != len(self.M0):
+            raise(ValueError, "M must be %d dimension" % len(self.M0))
+
+        S1 = [0]*len(self.S)
+
+
+        for i in range(len(self.S)):
+            if math.fsum(M) > 20:
+                S1[i] = 0
+            elif math.fsum(M) > -40:
+                S1[i] = math.sin(M[0]/25.0)*30.0
+            else:
+                S1[i] =0 + random.uniform(-60, 60)
+
+        self.S = tuple(S1)
+        self.M0 = M
 
 class SimpleDataSource(SimpleFunction):
 
@@ -148,7 +212,7 @@ def generate_data(function, num_sample=1000, randomize=True):
 
 if __name__ == '__main__':
 
-    func = SimpleFunction2(low_bound=(-80, -80 ), high_bound=(80, 80))
+    func = SimpleFunction3(low_bound=(-80, -80 ), high_bound=(80, 80))
     data, label = (generate_data(func, num_sample=10000, randomize=True))
 
     with open('SimpleData.pkl', 'wb') as data_pickle:
