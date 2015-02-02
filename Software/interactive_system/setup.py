ab88e4bc6a013fbd5340a5a7417ebe9cc393c573
tuzzer <matthew.tk.chan@gmail.com>
2015-02-02 15:11:58 -0500

    added pyusb and numpy as requirement

--- Software/interactive_system/setup.py
+++ Software/interactive_system/setup.py
@@ -10,4 +10,7 @@ setup(name='interactive_system',
       packages=['interactive_system'],
 	  package_dir={'interactive_system': 'interactive_system'},
       package_data={'interactive_system': ['protocol_config/*']},
+	  install_requires=[
+          'pyusb', 'numpy', 
+      ],
       zip_safe=False)
