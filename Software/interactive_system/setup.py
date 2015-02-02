c7fe48eac2d154c40fdfea0c9ff9908f2529f3ae
tuzzer <matthew.tk.chan@gmail.com>
2015-02-02 15:01:51 -0500

    added a setup file for the interactive-system package

---
+++ Software/interactive_system/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='interactive_system',
+      version='0.1',
+      description='Interface with Teensy control that control the interactive art sculpture at PBAI',
+      url='https://github.com/tuzzer/CBLA-Test-Bed/tree/master/Software/InteractiveSystem',
+      author='Matthew T.K. Chan',
+      author_email='matthew.chan@uwaterloo.ca',
+      license='MIT',
+      packages=['interactive_system'],
+	  package_dir={'interactive_system': 'interactive_system'},
+      package_data={'interactive_system': ['protocol_config/*']},
+      zip_safe=False)
