7e7938842741161517383ac7c42ba5773296d4db
tuzzer <matthew.tk.chan@gmail.com>
2015-03-16 18:11:55 -0400

    added pre-scripted behaviour defined the base class for a node

---
+++ Software/prescripted_behaviour/node.py
@@ -0,0 +1,29 @@
+import threading
+
+
+class Node(threading.Thread):
+
+    def __init__(self, output_vars: list, input_vars: list):
+
+        super(Node, self).__init__()
+
+        if not isinstance(output_vars, list) or not isinstance(input_vars, list):
+            raise TypeError('output_vars and input_vars must both be tuple!')
+
+        # constructing the list of output variables
+        self.out_var = output_vars
+
+        # constructing the list of input variables
+        self.in_var = input_vars
+
+    def run(self):
+
+        raise SystemError('Run must be defined in the child class')
+
+
+
+
+
+
+
+
