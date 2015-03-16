615ebb4a64540d57232c2742cac71686c6273ea0
tuzzer <matthew.tk.chan@gmail.com>
2015-03-16 18:11:55 -0400

    added pre-scripted behaviour defined the base class for a node

---
+++ Software/prescripted_behaviour/prescripted_behaviour.py
@@ -0,0 +1,10 @@
+from interactive_system import InteractiveCmd
+from interactive_system.InteractiveCmd import command_object
+
+class Prescripted_Behaviour(InteractiveCmd.InteractiveCmd):
+
+    # ========= the Run function for the prescripted behaviour system =====
+    def run(self):
+
+        print('system ran')
+
