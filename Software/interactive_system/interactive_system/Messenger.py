0f594ac3d2a15cd61fd96813eacbe00223072f7f
tuzzer <matthew.tk.chan@gmail.com>
2015-03-17 22:52:33 -0400

    Replace Sync_Barrier with Messenger Add lock to the InteractiveCmd base class

--- Software/interactive_system/interactive_system/Messenger.py
+++ Software/interactive_system/interactive_system/Messenger.py
@@ -5,6 +5,7 @@ from time import sleep
 
 from . import InteractiveCmd
 
+
 class Messenger(threading.Thread):
 
     def __init__(self, interactive_cmd: InteractiveCmd.InteractiveCmd,
@@ -13,21 +14,46 @@ class Messenger(threading.Thread):
         self.t0 = 0.0
         self.msg_period = msg_period
         self.cmd = interactive_cmd
+        self.cmd_q = queue.Queue()
+
+        self.__sample = None
+        self.sample_inputs(msg_period)
 
         super(Messenger, self).__init__(daemon=True)
 
-
+    @property
+    def sample(self):
+        return self.__sample
 
     def run(self):
 
         self.t0 = clock()
         while True:
-            self.cmd.send_commands()
-            sleep(self.msg_period - (clock() - self.t0))
+
+            while not self.cmd_q.empty():
+                msg = self.cmd_q.get_nowait()
+                with self.cmd.lock:
+                    self.cmd.enter_command(msg)
+
+            with self.cmd.lock:
+                self.cmd.send_commands()
+
+            self.sample_inputs(self.msg_period)
+
+            sleep(max(0, self.msg_period - (clock() - self.t0)))
+
+            #print('Update time = %f' % (clock() - self.t0))
             self.t0 = clock()
 
+    def load_message(self, msg: InteractiveCmd.command_object):
 
+        self.cmd_q.put_nowait(msg)
 
+    def sample_inputs(self, timeout_max=0.0):
 
+        with self.cmd.lock:
+            self.cmd.update_input_states(self.cmd.teensy_manager.get_teensy_name_list())
 
-
+            self.__sample = self.cmd.get_input_states(self.cmd.teensy_manager.get_teensy_name_list(), ('all',),
+                                                      timeout=max(0.1, timeout_max))
+            # print(self.__sample['test_teensy_3'][0]['protocell_0_als_state'])
