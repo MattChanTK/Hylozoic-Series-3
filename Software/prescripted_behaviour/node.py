8e6c0c87f7d852735d556b6b1625f19a0c416f98
tuzzer <matthew.tk.chan@gmail.com>
2015-03-22 22:53:59 -0400

    added IR proximity sensor and Tentacle for prescripted behaviours

--- Software/prescripted_behaviour/node.py
+++ Software/prescripted_behaviour/node.py
@@ -1,38 +1,183 @@
 import threading
-
+from interactive_system import Messenger
+from interactive_system.InteractiveCmd import command_object
+from time import sleep
 
 class Node(threading.Thread):
 
-    def __init__(self, output_vars: list, input_vars: list):
+    def __init__(self, messenger: Messenger.Messenger, node_name=None):
 
         super(Node, self).__init__(daemon=True)
 
-        if not isinstance(output_vars, list) or not isinstance(input_vars, list):
-            raise TypeError('output_vars and input_vars must both be tuple!')
-
-        # constructing the list of output variables
-        self.out_var = output_vars
+        self.node_name = 'generic'
+        self.messenger = messenger
 
         # constructing the list of input variables
-        self.in_var = input_vars
+        self.in_var = dict()
+
+        # constructing the list of output variables
+        self.out_var = dict()
+
+        if isinstance(node_name, str):
+            self.node_name = node_name
+
+    @property
+    def in_var_list(self) -> tuple:
+        return tuple(self.in_var.keys())
+
+    @property
+    def get_out_var_list(self) -> tuple:
+        return tuple(self.out_var.keys())
+
+    def run(self):
+        raise SystemError('Run must be defined in the child class')
+
+    def read_sample(self, print_warning=False):
+
+        sample = self.messenger.sample
+
+        if print_warning:
+            # if the first sample read was unsuccessful, just return the default value
+            if sample is None:
+                print("unsuccessful read")
+
+            # if any one of those data wasn't new, it means that it timed out
+            for teensy_name, sample_teensy in sample.items():
+                if not sample_teensy[1]:
+                    print(teensy_name + " has timed out")
+
+        return sample
+
+    def send_output_cmd(self, teensy, *output):
+
+        if output:
+            if not isinstance(teensy, str):
+                raise TypeError('Teensy name must be string!')
+
+            cmd_obj = command_object(teensy, msg_setting=1)
+        else:
+            return
+
+        for output_name, value in output:
+            if isinstance(value, Var):
+                value = value.val
+            cmd_obj.add_param_change(output_name, value)
+
+        self.messenger.load_message(cmd_obj)
+
+
+class Var(object):
+
+    def __init__(self, val=0):
+        self.__val = val
+
+    @property
+    def val(self):
+        return self.__val
+
+    @val.setter
+    def val(self, new_val):
+        self.__val = new_val
+
+
+class Test_Node(Node):
+
+    def __init__(self, messenger: Messenger.Messenger, input_addr):
+
+        super(Test_Node, self).__init__(messenger)
+
+        self.in_var['ir_sensor_1'] = input_addr
 
     def run(self):
 
-        raise SystemError('Run must be defined in the child class')
+        while True:
+            print('[%s] %s: %f' % (self.node_name, 'sensor_out', self.in_var['ir_sensor_1'].val))
+            sleep(0.5)
 
 
 class Tentacle(Node):
 
+    def __init__(self, messenger: Messenger.Messenger, teensy_name: str, tentacle_num: int, ir_0: Var, ir_1: Var):
+
+        if not isinstance(teensy_name, str):
+            raise TypeError('teensy_name must be a string!')
+
+        self.teensy_name = teensy_name
+
+        if not isinstance(tentacle_num, int):
+            raise TypeError('Tentacle number must an integer!')
+
+        self.tentacle_name = 'tentacle_%d_arm_motion_on' % tentacle_num
+
+        super(Tentacle, self).__init__(messenger, node_name='Tentacle %d' % tentacle_num)
+
+        # defining the input variables
+        self.in_var['ir_sensor_0'] = ir_0
+        self.in_var['ir_sensor_1'] = ir_1
+
+        # defining the output variables
+        self.out_var['tentacle_out'] = Var(0)
+        self.out_var['reflex_out_0'] = Var(0)
+        self.out_var['reflex_out_1'] = Var(0)
+
+
     def run(self):
 
-        pass
+        while True:
+            output_changed = False
+            if self.in_var['ir_sensor_1'].val > 1000 and self.out_var['tentacle_out'].val ==0:
+                self.out_var['tentacle_out'].val = 3
+                self.out_var['reflex_out_0'].val = 100
+                self.out_var['reflex_out_1'].val = 100
+                output_changed = True
+
+            elif self.in_var['ir_sensor_1'].val <= 1000 and self.out_var['tentacle_out'].val > 0:
+
+                self.out_var['tentacle_out'].val = 0
+                self.out_var['reflex_out_0'].val = 0
+                self.out_var['reflex_out_1'].val = 0
+                output_changed = True
+
+            if output_changed:
+
+                self.send_output_cmd(self.teensy_name,
+                                     (self.tentacle_name, self.out_var['tentacle_out']),
+                                     (self.tentacle_name, self.out_var['reflex_out_0']),
+                                     (self.tentacle_name, self.out_var['reflex_out_1']))
+
+            print('%d, %d, %d' % (self.out_var['tentacle_out'].val,
+                                  self.out_var['reflex_out_0'].val,
+                                  self.out_var['reflex_out_1'].val))
+
+
+            sleep(1)
 
 class IR_Proximity_Sensor(Node):
 
+    def __init__(self, messenger: Messenger.Messenger, teensy_name: str, sensor_name: str):
+        super(IR_Proximity_Sensor, self).__init__(messenger, node_name='ir sensor')
+
+        if not isinstance(teensy_name, str):
+            raise TypeError('teensy_name must be a string!')
+
+        if not isinstance(sensor_name, str):
+            raise TypeError('sensor_name must be a string!')
+
+        self.teensy_name = teensy_name
+        self.sensor_name = sensor_name
+
+        # define output variables
+        self.out_var['sensor_raw'] = Var(0)
+
+
     def run(self):
 
-        pass
+        while True:
+            sample = self.read_sample()
+            self.out_var['sensor_raw'].val = sample[self.teensy_name][0][self.sensor_name]
 
+            #print('[%s] %s: %f' % (self.node_name, 'sensor_out', self.out_var['sensor_raw'].val))
+            sleep(self.messenger.estimated_msg_period)
 
 
 
--- Software/prescripted_behaviour/prescripted_behaviour.py
+++ Software/prescripted_behaviour/prescripted_behaviour.py
@@ -1,10 +1,27 @@
 from interactive_system import InteractiveCmd
-from interactive_system.InteractiveCmd import command_object
-
+from interactive_system import Messenger
+import node
 class Prescripted_Behaviour(InteractiveCmd.InteractiveCmd):
 
     # ========= the Run function for the prescripted behaviour system =====
     def run(self):
 
-        print('system ran')
+        messenger = Messenger.Messenger(self, 0.03)
+        messenger.start()
+
+
+        ir_sensor_0 = node.IR_Proximity_Sensor(messenger, 'test_teensy_3', 'tentacle_0_ir_0_state')
+        ir_sensor_0.start()
+
+        ir_sensor_1 = node.IR_Proximity_Sensor(messenger, 'test_teensy_3', 'tentacle_0_ir_1_state')
+        ir_sensor_1.start()
+
+
+        # test_node = node.Test_Node(messenger, ir_sensor_0.out_var['sensor_raw'])
+        # test_node.start()
+
+        tentacle_0 = node.Tentacle(messenger, teensy_name='test_teensy_3', tentacle_num=0,
+                                   ir_0=ir_sensor_0.out_var['sensor_raw'], ir_1=ir_sensor_0.out_var['sensor_raw'])
+        tentacle_0.start()
+        print('System Initialized')
 
