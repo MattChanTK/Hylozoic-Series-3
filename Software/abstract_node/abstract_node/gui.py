d5f5928e020c7110990b1a391308ad5842f9fd9e
tuzzer <matthew.tk.chan@gmail.com>
2015-04-07 16:22:40 -0400

    added the messenger frame that shows the messenger's update period

--- Software/abstract_node/abstract_node/gui.py
+++ Software/abstract_node/abstract_node/gui.py
@@ -14,21 +14,35 @@ class Main_GUI(Node):
         self.root = tk.Tk()
         self.frame_list = []
 
+        self._add_messenger_frame(messenger)
+
+
     def add_frame(self, *child_frame):
 
         self.frame_list += child_frame
 
+    def _add_messenger_frame(self, messenger):
+
+        # add the messenger frame automatically
+        self.messenger_frame = Messenger_Frame(self.root, messenger)
+        self.frame_list.append(self.messenger_frame)
 
     def run(self):
         self.pack_frames()
         for frame in self.frame_list:
             frame.run()
             frame.updateFrame()
+
         self.root.mainloop()
 
     def pack_frames(self):
+
         for frame in self.frame_list:
-            frame.pack()
+            if isinstance(frame, Messenger_Frame):
+                frame.pack(side=tk.BOTTOM)
+            else:
+                frame.pack(side=tk.LEFT)
+
 
 
 class Display_Frame(tk.Frame):
@@ -38,44 +52,63 @@ class Display_Frame(tk.Frame):
         super(Display_Frame, self).__init__(tk_master)
 
         self.node_list = node_list
+
+        self._construct_frame()
+
+    def _construct_frame(self):
+
         self.status_text = OrderedDict()
         self.action_text = OrderedDict()
-        self.status_frame = tk.Frame()
-        self.action_frame = tk.Frame()
+        self.status_frame = tk.Frame(self)
+        self.action_frame = tk.Frame(self)
 
         self.status_frame.pack(side=tk.LEFT)
         self.action_frame.pack(side=tk.LEFT)
 
-        for name, node in node_list.items():
+        # header label
+        self.status_frame_label = tk.Label(self.status_frame, text="Sensor Readings", font=("Helvetica", 14))
+        self.status_frame_label.grid(row=0, columnspan=2)
+        self.action_frame_label = tk.Label(self.action_frame, text="Actuator Outputs", font=("Helvetica", 14))
+        self.action_frame_label.grid(row=0, columnspan=2)
+
+        # contents
+        for name, node in self.node_list.items():
             if isinstance(node, Input_Node):
                 for var_name in node.out_var_list:
-                    self.status_text[(name, var_name)] = tk.Label(self.status_frame,
-                                                                  text="%s = %d" % (name, node.out_var[var_name].val),
-                                                                  fg='blue')
+                    self.status_text[(name, var_name)] = [tk.Label(self.status_frame,text="%s" % name, fg='blue'),
+                                                          tk.Label(self.status_frame,
+                                                                   text="###",
+                                                                   fg='blue', width='8', anchor=tk.E, justify=tk.RIGHT)]
 
             elif isinstance(node, Output_Node) or isinstance(node, Frond):
                 for var_name in node.in_var_list:
-                    self.action_text[(name, var_name)] = tk.Label(self.action_frame,
-                                                                  text="%s = %d" % (name, node.in_var[var_name].val),
-                                                                  fg='red')
-
-
+                    self.action_text[(name, var_name)] = [tk.Label(self.action_frame, text="%s" % name, fg='red'),
+                                                          tk.Label(self.action_frame,
+                                                                   text="###",
+                                                                   fg='red', width='8', anchor=tk.E, justify=tk.RIGHT)]
 
     def run(self):
 
+        row_id = 2
         for label in self.status_text.values():
-            label.pack(fill=tk.X, padx=10)
+            label[0].grid(column=0, row=row_id, ipadx=10, sticky=tk.W)
+            label[1].grid(column=1, row=row_id, ipadx=10, sticky=tk.E)
+            row_id += 1
+
         for label in self.action_text.values():
-            label.pack(fill=tk.X, padx=10)
+            label[0].grid(column=0, row=row_id, ipadx=10, sticky=tk.W)
+            label[1].grid(column=1, row=row_id, ipadx=10, sticky=tk.E)
+            row_id += 1
 
     def updateFrame(self):
         for name, label in self.status_text.items():
-            msg = "%s.%s:\t\t%d" % (name[0], name[1], self.node_list[name[0]].out_var[name[1]].val)
-            label["text"] = msg
+
+            msg = "%s" % str(self.node_list[name[0]].out_var[name[1]].val)[:6]
+            label[1]["text"] = msg
 
         for name, label in self.action_text.items():
-            msg = "%s.%s:\t%d" % (name[0], name[1], self.node_list[name[0]].in_var[name[1]].val)
-            label["text"] = msg
+            msg = "%s" % str(self.node_list[name[0]].in_var[name[1]].val)[:6]
+            label[1]["text"] = msg
 
         self.after(500, self.updateFrame)
         self.update()
@@ -121,6 +154,28 @@ class Control_Frame(tk.Frame):
 
         self.after(100, self.updateFrame)
         self.update()
+
+class Messenger_Frame(tk.Frame):
+
+    def __init__(self, tk_master, messenger):
+        super(Messenger_Frame, self).__init__(tk_master)
+
+        self.messenger = messenger
+
+        # frame for showing the messenger speed
+        self.messenger_time_label = tk.Label(self, text="Messenger Update Period: ###", fg='green',
+                                             font=("Helvetica", 10), anchor=tk.E, justify=tk.RIGHT)
+
+
+    def run(self):
+        self.messenger_time_label.pack(side=tk.RIGHT, anchor=tk.E)
+
+    def updateFrame(self):
+
+        self.messenger_time_label["text"] = "Messenger Update Period: %.4fs" % self.messenger.estimated_msg_period
+        self.after(100, self.updateFrame)
+        self.update()
+
 
 
 if __name__ == '__main__':
