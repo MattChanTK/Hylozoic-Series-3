63a1bcb9f38a0ae2f41a51b6d6ff91274324cb5c
tuzzer <matthew.tk.chan@gmail.com>
2015-05-14 00:38:24 -0400

    move the running average, and pseudodifferentiation to the abstract node module

--- Software/abstract_node/abstract_node/low_level_node.py
+++ Software/abstract_node/abstract_node/low_level_node.py
@@ -1,8 +1,13 @@
 __author__ = 'Matthew'
 
 from time import clock
+from collections import deque
+from copy import copy
+import numpy as np
+
 from abstract_node.node import *
 from interactive_system import Messenger
+from .simple_data_collect import *
 
 
 class Frond(Node):
@@ -114,6 +119,147 @@ class Frond_SMA(Simple_Node):
 
             self.controller.update(self.in_var['temp_ref'])
             sleep(self.messenger.estimated_msg_period*2)
+
+
+class LED_Driver(Simple_Node):
+
+    def __init__(self, messenger: Messenger, node_name='LED_Driver',
+                 led_ref: Var=Var(0), led_out: Var=Var(0), step_period=0):
+
+        super(LED_Driver, self).__init__(messenger, node_name='%s' % node_name, output=led_out,
+                                         led_ref=led_ref)
+
+        self.step_period = step_period
+
+    def run(self):
+
+        while self.alive:
+
+            if self.out_var['output'].val < self.in_var['led_ref'].val:
+                self.out_var['output'].val += max(1, int(self.out_var['output'].val * 0.1))
+                sleeping_time = max(0, max(self.messenger.estimated_msg_period * 2, self.step_period))
+
+            elif self.out_var['output'].val > self.in_var['led_ref'].val:
+                self.out_var['output'].val -= max(1, int(self.out_var['output'].val * 0.1))
+                sleeping_time = max(0, max(self.messenger.estimated_msg_period * 2, self.step_period))
+            else:
+                sleeping_time = max(0,self.messenger.estimated_msg_period * 2)
+
+            sleep(sleeping_time)
+
+
+
+class Data_Collector_Node(Node):
+
+    def __init__(self, messenger: Messenger, node_name='data_collector', file_header='sys_id_data',
+                 **variables):
+        super(Data_Collector_Node, self).__init__(messenger, node_name=node_name)
+
+        # defining the input variables
+        for var_name, var in variables.items():
+            if isinstance(var, Var):
+                self.in_var[var_name] = var
+            else:
+                raise TypeError("Variables must be of Var type!")
+
+        self.data_collect = SimpleDataCollector(file_header=file_header)
+
+    def run(self):
+
+        self.data_collect.start()
+
+        loop_count = 0
+        while self.alive:
+            loop_count += 1
+            data_packets = defaultdict(OrderedDict)
+
+            for var_name, var in self.in_var.items():
+                var_split = var_name.split('.')
+                teensy_name = var_split[0]
+                device_name = var_split[1]
+                point_name = var_split[2]
+                data_packets['%s.%s' % (teensy_name, device_name)][point_name] = copy(var.val)
+
+
+            for packet_name, data_packet in data_packets.items():
+                data_packet['time'] = datetime.now()
+                data_packet['step'] = loop_count
+
+                self.data_collect.append_data_packet(packet_name, data_packet)
+
+            sleep(self.messenger.estimated_msg_period*2)
+
+        self.data_collect.end_data_collection()
+        self.data_collect.join()
+
+
+
+class Pseudo_Differentiation(Node):
+
+    def __init__(self, messenger: Messenger, node_name='Pseudo_Differentiation',
+                 input_var: Var=Var(0),
+                 diff_gap=1, smoothing=1, step_period=0.1):
+
+        if not isinstance(input_var, Var):
+            raise TypeError("input_var must be of type Var!")
+
+        super(Pseudo_Differentiation, self).__init__(messenger, node_name=node_name)
+
+        self.diff_gap = diff_gap
+        self.smoothing = smoothing
+
+        self.in_var['input'] = input_var
+        self.out_var['output'] = Var()
+        self.out_var['output'].val = 0
+
+        self.input_deque = deque(maxlen=(self.smoothing+self.diff_gap))
+        self.step_period = step_period
+
+    def run(self):
+
+        while self.alive:
+
+            self.input_deque.append(copy(self.in_var['input'].val))
+
+            # calculate differences
+            val_list = list(self.input_deque)
+            if len(val_list) >= self.diff_gap + 1:
+                self.out_var['output'].val = np.mean(val_list[-self.smoothing:]) \
+                                             - np.mean(val_list[:-self.diff_gap])
+            sleep(self.step_period)
+
+
+class Running_Average(Node):
+
+    def __init__(self, messenger: Messenger, node_name='Pseudo_Differentiation',
+                 input_var: Var=Var(0),
+                 avg_window=1, step_period=0.1):
+
+
+        if not isinstance(input_var, Var):
+            raise TypeError("input_var must be of type Var!")
+
+        super(Running_Average, self).__init__(messenger, node_name=node_name)
+
+        self.avg_window = avg_window
+
+        self.in_var['input'] = input_var
+        self.out_var['output'] = Var()
+        self.out_var['output'].val = 0
+
+        self.input_deque = deque(maxlen=avg_window)
+        self.step_period = step_period
+
+    def run(self):
+        while self.alive:
+
+            self.input_deque.append(copy(self.in_var['input'].val))
+
+            self.out_var['output'].val = np.mean(self.input_deque)
+
+            sleep(self.step_period)
+
+
 
 
 
