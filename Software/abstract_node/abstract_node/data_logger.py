4ac3449a5ef6633c6e2d9e228ac86998bc179f7f
tuzzer <matthew.tk.chan@gmail.com>
2015-07-08 10:21:48 -0400

    WIP: using shelve to replace pickling dict object

--- Software/abstract_node/abstract_node/data_logger.py
+++ Software/abstract_node/abstract_node/data_logger.py
@@ -18,43 +18,109 @@ import shelve
 
 class DataLogger(threading.Thread):
 
-    def __init__(self, log_dir='log_data', log_header='generic_data'):
+    def __init__(self, log_dir='log_data', log_header='generic_data', log_timestamp=None, log_path=None):
 
-        log_dir_path = os.path.join(os.getcwd(), log_dir, log_header)
+        now = time.strftime("%y-%m-%d_%H-%M-%S", time.localtime())
+
+        # if a specific path is specified
+        if isinstance(log_path, str) and os.path.isdir(log_path):
+            log_dir_path = log_path
+            log_name = os.path.split(log_dir_path)[-1]
+        else:
+            if log_timestamp == None:
+                log_timestamp = now
+
+            log_name =  '%s_%s' % (log_header, log_timestamp)
+            log_dir_path = os.path.join(os.getcwd(), log_dir, log_name)
+
         # check if log directory even exist
         if not os.path.exists(log_dir_path):
             # create directory if does not exist
             os.makedirs(log_dir_path)
 
         # check if the log file exist
-        log_path = os.path.join(log_dir_path, log_header)
-        self.log_file = shelve.open(log_path, protocol=3, writeback=False)
+        log_path = os.path.join(log_dir_path, log_name)
+        self.log_index_file = shelve.open(log_path, protocol=3, writeback=False)
 
         # if from previous session
-        if 'file_num' in self.log_file:
-            self.log_file['file_num'] += 1
-            self.data_file['session_start_time'].append(datetime.now())
+        if self.log_index_file:
 
-        # if having no data
+            try:
+                self.log_index_file['num_session'] += 1
+            except KeyError:
+                raise KeyError('log file is corrupted!')
+
+            curr_session = self.log_index_file['num_session']
         else:
-            self.data_file = dict()
-            self.data_file['data'] = defaultdict(list)
-
-            now = time.strftime("%y-%m-%d_%H-%M-%S", time.localtime())
-            self.data_file['file_name'] = '%s_%s' % (file_header, now)
-            self.data_file['file_num'] = 1
-
-            # put time stamp on the packet
-            self.data_file['session_start_time'] = [datetime.now()]
-
-        self.packet_queue = Queue()
-        super(DataLogger, self).__init__(name='SimpleDataLogger', daemon=False)
+            curr_session = 1
+            self.log_index_file['time_created'] = now
+            self.log_index_file['num_session'] = curr_session
 
 
+        self.log_index_file[str(curr_session)] =  'session_%03d' % curr_session
+
+        # create the session's directory
+        session_dir_path = os.path.join(log_dir_path, self.log_index_file[str(curr_session)])
+        os.mkdir(session_dir_path)
+
+        # open the shelve for the session
+        session_path = os.path.join(session_dir_path, self.log_index_file[str(curr_session)])
+        self.session_shelf = shelve.open(session_path, protocol=3, writeback=False)
+        self.session_shelf['time_created'] = now
+        self.session_shelf['session_id'] = curr_session
+
+        # queue for packet to come in
+        self.__packet_queue = Queue()
+
+        # variables
+        self.__program_terminating = False
+
+        # parameters
+        self.sleep_time = 0.001
+
+
+        super(DataLogger, self).__init__(name='DataLogger', daemon=False)
+
+    def run(self):
+
+        while not self.__program_terminating or not self.__packet_queue.empty():
+
+            # saving run-time data to memory
+            if not self.__packet_queue.empty():
+                node_name, packet_data = self.__packet_queue.get_nowait()
+                self.session_shelf[(node_name, 'data', time.localtime())] = packet_data
+
+            if self.__program_terminating:
+                self.sleep_time = 0.0
+
+            sleep(self.sleep_time)
+
+    def append_data_packet(self, node_name, data_packet):
+        self.__packet_queue.put((node_name, copy(data_packet)))
+
+    def end_data_collection(self):
+        self.__program_terminating = True
 
 # test script
 if __name__ == '__main__':
 
-    data_log_folder = 'data_log'
-    data_logger = DataLogger(os.path.join(os.getcwd(), data_log_folder))
+    create_new_log = True
+    log_dir_path = os.path.join(os.getcwd(), 'data_log')
+
+    if create_new_log:
+        latest_log_dir = None
+    else:
+        # use the latest data log
+        all_log_dir = []
+        for dir in os.listdir(log_dir_path):
+            dir_path = os.path.join(log_dir_path, dir)
+            if os.path.isdir(dir_path):
+                all_log_dir.append(dir_path)
+        latest_log_dir = max(all_log_dir, key=os.path.getmtime)
+
+    data_logger = DataLogger(os.path.join(os.getcwd(), log_dir_path), log_path=latest_log_dir)
+    data_logger.start()
+    for i in range(1000):
+
+        data_logger.append_data_packet('test_node', (i, i/2, i*2))
 
