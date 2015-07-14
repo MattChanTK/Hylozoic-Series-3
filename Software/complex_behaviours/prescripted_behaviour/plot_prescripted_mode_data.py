0ca9c74365b106bdd79cb5b6bb5a9ed456997015
tuzzer <matthew.tk.chan@gmail.com>
2015-07-14 14:32:52 -0400

    removed the set filename for the log

--- Software/complex_behaviours/prescripted_behaviour/plot_prescripted_mode_data.py
+++ Software/complex_behaviours/prescripted_behaviour/plot_prescripted_mode_data.py
@@ -6,7 +6,7 @@ from abstract_node.simple_data_plot import *
 if __name__ == '__main__':
     log_dir = os.path.join(os.getcwd(), 'data_log')
 
-    plotter = Plotter(log_dir=log_dir, log_header='prescripted_mode', log_name='prescripted_mode_data_2015-07-13_12-39-30')
+    plotter = Plotter(log_dir=log_dir, log_header='prescripted_mode') #, log_name='prescripted_mode_data_2015-07-13_12-39-30')
     plotter.plot()
     # plotter.update_plot()
     plotter.save_all_plots(plotter.log_name)
