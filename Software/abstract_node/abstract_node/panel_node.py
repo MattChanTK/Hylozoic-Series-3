f1faaac930ef8dea910f031dbc3753006d5d8009
tuzzer <matthew.tk.chan@gmail.com>
2015-08-25 15:54:30 -0400

    added the panel_node for GUI info

--- Software/abstract_node/abstract_node/panel_node.py
+++ Software/abstract_node/abstract_node/panel_node.py
@@ -9,15 +9,20 @@ from interactive_system import Messenger
 
 class UserStudyPanel(Node):
 
-    def __init__(self, messenger: Messenger, node_name="user_study_panel", ):
+    def __init__(self, messenger: Messenger, node_name="user_study_panel", **in_vars):
 
         super(UserStudyPanel, self).__init__(messenger, node_name=node_name)
 
-        self.datetime_str_fmt_us =  "%Y-%m-%d %H:%M:%Ss:%fus" # "%Y-%m-%d_%H-%M-%S-%f"
+        self.datetime_str_fmt_us = "%Y-%m-%d %H:%M:%S:%f" # "%Y-%m-%d_%H-%M-%S-%f"
 
         # output variables
         self.out_var['sample_number'] = Var(0)
         self.out_var['curr_time'] = Var(datetime.now().strftime(self.datetime_str_fmt_us))
+
+        # input variables
+        for in_var_name, in_var in in_vars.items():
+            if isinstance(in_var, Var):
+                self.in_var[in_var_name] = in_var
 
     def run(self):
 
