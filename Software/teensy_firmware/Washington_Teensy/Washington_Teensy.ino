fe9aca78d8a2625b0de15708fee9bde138ee4c8d
tuzzer <matthew.tk.chan@gmail.com>
2015-10-28 09:44:28 -0400

    updated the Sound module code

--- Software/teensy_firmware/Washington_Teensy/Washington_Teensy.ino
+++ Software/teensy_firmware/Washington_Teensy/Washington_Teensy.ino
@@ -4,10 +4,10 @@
 //===========================================================================
 
 //===== INITIALIZATION =====
-//WashingtonCricketNode teensy_unit(0, 2, 5, 3);
+WashingtonCricketNode teensy_unit(0, 2, 5, 3);
 //WashingtonFinCricketNode teensy_unit(1, 3, 4, 0, 2, 5);
 //WashingtonFinNode teensy_unit(1, 3, 4, 0, 2, 5);
-WashingtonSoundNode teensy_unit(0, 1, 2, 3, 4, 5);
+//WashingtonSoundNode teensy_unit(0, 1, 2, 3, 4, 5);
 
 //check for new messages
 void check_msg(){
