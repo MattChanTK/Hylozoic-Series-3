579a1e8520ba8b775b12df7549e419953842ea88
tuzzer <matthew.tk.chan@gmail.com>
2015-11-16 13:18:22 -0500

    started on the node_spatial_map

---
+++ Software/complex_behaviours/cbla/node_spatial_map.py
@@ -0,0 +1,13 @@
+__author__ = 'Matthew'
+
+
+class NodeSpatialMap(object):
+
+    def __init__(self):
+        pass
+
+    def add_node(self, node_name, x, y, z=0):
+        pass
+
+    def calc_distance(self, node_name_1, node_name_2):
+        return 0
