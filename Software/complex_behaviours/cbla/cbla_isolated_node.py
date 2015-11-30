a4b68954863a1c2c5835b3bf39f9ff3c2d994908
tuzzer <matthew.tk.chan@gmail.com>
2015-11-30 18:20:47 -0500

    commented out using linear_regression as model

--- Software/complex_behaviours/cbla/cbla_isolated_node.py
+++ Software/complex_behaviours/cbla/cbla_isolated_node.py
@@ -20,6 +20,7 @@ class Isolated_Light_Node(CBLA_Generic_Node):
         learner_config['reward_smoothing'] = 3
         learner_config['kga_delta'] = 2
         learner_config['kga_tau'] = 4
+        # learner_config['prediction_model'] = linear_model.LinearRegression()
         learner_config['prediction_model'] = linear_model.Lasso(alpha=0.02,
                                                                 positive=False,
                                                                 normalize=False,
@@ -45,6 +46,7 @@ class Isolated_HalfFin_Node(CBLA_Generic_Node):
         learner_config['reward_smoothing'] = 1
         learner_config['kga_delta'] = 1
         learner_config['kga_tau'] = 2
+        # learner_config['prediction_model'] = linear_model.LinearRegression()
         learner_config['prediction_model'] = linear_model.Lasso(alpha=0.02,
                                                                 normalize=False,
                                                                 warm_start=True,
@@ -69,6 +71,7 @@ class Isolated_Reflex_Node(CBLA_Generic_Node):
         learner_config['reward_smoothing'] = 10
         learner_config['kga_delta'] = 2
         learner_config['kga_tau'] = 5
+        # learner_config['prediction_model'] = linear_model.LinearRegression()
         learner_config['prediction_model'] = linear_model.Lasso(alpha=0.02,
                                                                 normalize=False,
                                                                 warm_start=True,
