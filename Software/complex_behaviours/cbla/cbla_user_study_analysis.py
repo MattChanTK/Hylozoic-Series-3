a43e774304559eb933cbc15d70adad4da9ce9a21
tuzzer <matthew.tk.chan@gmail.com>
2015-12-09 10:44:16 -0500

    added code that export data of different window sizes to excel format remove the code from the "plot" function which isn't necessary fixed bug that will mess up data when more then two types of data types are being used (namely, M and out_vars)

--- Software/complex_behaviours/cbla/cbla_user_study_analysis.py
+++ Software/complex_behaviours/cbla/cbla_user_study_analysis.py
@@ -7,12 +7,23 @@ from cbla_generic_node import *
 
 if __name__ == '__main__':
 
-    log_dir = os.path.join(os.getcwd(), 'cbla_log', 'study_1')
+    for i in range(1, 9):
+        log_dir = os.path.join(os.getcwd(), 'cbla_log', 'study_%d' % i)
 
-    plotter = UserStudyPlotter(log_dir=log_dir,
-                               packet_types=(CBLA_Base_Node.cbla_data_type_key, CBLA_Base_Node.prescripted_data_type_key),
-                               info_types=(CBLA_Base_Node.cbla_state_type_key, CBLA_Base_Node.cbla_label_name_key,))
-    plotter.plot()
+        plotter = UserStudyPlotter(log_dir=log_dir, study_number=i, session_num=2,
+                                   packet_types=(CBLA_Base_Node.cbla_data_type_key, CBLA_Base_Node.prescripted_data_type_key),
+                                   info_types=(CBLA_Base_Node.cbla_state_type_key, CBLA_Base_Node.cbla_label_name_key,))
+
+        win_periods = (0.5, 1.0, 2.0, 5.0, 10.0, 20.0)
+        for win_period in win_periods:
+
+            plotter.node_active_array = plotter.compute_node_activation_array(win_period=win_period)
+
+            plotter.write_node_activation_array_to_excel(file_name="study_%d (%.2fs).xls" % (plotter.study_number, win_period),
+                                                          save_to_dir="user_study_excel_data")
+
+
+    # plotter.plot(win_period=2.0)
     # plotter.update_plot()
-    plotter.save_all_plots(plotter.log_name)
+    # plotter.save_all_plots(plotter.log_name)
     # plotter.show_plots()
--- Software/complex_behaviours/cbla/cbla_user_study_plotter.py
+++ Software/complex_behaviours/cbla/cbla_user_study_plotter.py
@@ -2,38 +2,29 @@ __author__ = 'Matthew'
 from cbla_engine import cbla_data_plotter as cdp
 from collections import defaultdict
 import numpy as np
+import xlwt
+import xlrd
+import os
 
 
 class UserStudyPlotter(cdp.CBLA_DataPlotter):
 
-    def __init__(self, log_dir, log_header='cbla', packet_types=(), info_types=(), session_num=1):
+    def __init__(self, log_dir, study_number, session_num=2, log_header='cbla', packet_types=(), info_types=(),):
 
         self.node_active_array = None
+        self.win_period = None
+        self.study_number = study_number
 
         self.session_num = session_num
         super(UserStudyPlotter, self).__init__(log_dir=log_dir, log_header=log_header,
                                                packet_types=packet_types, info_types=info_types)
 
     def _construct_plot_objects(self):
-
-        session_data = self.data[self.session_num-1]
-        #
-        # for node_name, node_data in session_data.items():
-        #
-        #     self.plot_objects[(self.session_num, node_name, 'history')] = cdp.CBLA_PlotObject(fig_title='History Plot - %s (S%d)' % (node_name, self.session_num))
-        #
-        # self.plot_objects[(self.session_num, 'metrics')]  = cdp.PlotObject(fig_num=len(self.plot_objects)+1,
-        #                                                               fig_title='S%d - Metrics' % self.session_num)
+        pass
 
     def plot(self):
 
-        self.node_active_array = self.compute_node_activation_array()
-
-        for node_name, node_data in self.node_active_array.items():
-            print(node_name)
-            print(node_data)
-            print(np.mean(node_data, axis=0))
-            print()
+        pass
 
     # Compute an array of outputs for each Node wit the specified window period
     # if multiple samples are in one window, the average will be taken.
@@ -55,48 +46,56 @@ class UserStudyPlotter(cdp.CBLA_DataPlotter):
 
             node_array = []
 
-            # only extract the data types that represent output
-            for data_type, data_val in node_data.items():
+            # check if it is prescripted first or CBLA first
+            cbla_first = node_data['M']['x'][0] < node_data['out_vars']['x'][0]
+            # print("CBLA first?  ", cbla_first)
 
-                if data_type == 'M' or data_type == 'out_vars':
+            if cbla_first:
+                data_types = ('M', 'out_vars')
+            else:
+                data_types = ('out_vars', 'M')
 
-                    k = 0
-                    window_t = 0.0
+            # extracting the data in the right order
 
-                    # number of data points for this variable
-                    num_data_pt = len(data_val['x'])
+            window_t = 0.0
+            for data_type in data_types:
+                k = 0
 
-                    # temporary data buffer for a window
-                    win_data = []
+                data_val = node_data[data_type]
 
+                # number of data points for this variable
+                num_data_pt = len(data_val['x'])
 
-                    # iterate through every data point
-                    while k < num_data_pt:
+                # temporary data buffer for a window
+                win_data = []
 
-                        # if the time for this data point is within the current window
-                        if data_val['x'][k] < window_t + win_period:
-                            # append the value of the data point to the data buffer
-                            win_data.append(data_val['y'][k])
-                            # increment k to move onto the next data point
-                            k += 1
-                        # if the time for this data point is beyond this window
-                        else:
-                            # average the data in the buffer
-                            if len(win_data) > 0:
-                                # save the average in the node_array
-                                node_array.append((window_t, np.mean(win_data)))
-                            else:  # if there's no value in that window
-                                if len(node_array) > 0:
-                                    prev_win_avg = node_array[-1][1]
-                                else: # if there isn't any previous value
-                                    prev_win_avg = 0.0
-                                # save the previous average in the node_array
-                                node_array.append((window_t, prev_win_avg))
+                # iterate through every data point
+                while k < num_data_pt:
 
-                            # increment the current window's time
-                            window_t += win_period
-                            # empty the win_data buffer
-                            win_data = []
+                    # if the time for this data point is within the current window
+                    if data_val['x'][k] < window_t + win_period:
+                        # append the value of the data point to the data buffer
+                        win_data.append(data_val['y'][k])
+                        # increment k to move onto the next data point
+                        k += 1
+                    # if the time for this data point is beyond this window
+                    else:
+                        # average the data in the buffer
+                        if len(win_data) > 0:
+                            # save the average in the node_array
+                            node_array.append((window_t, np.mean(win_data)))
+                        else:  # if there's no value in that window
+                            if len(node_array) > 0:
+                                prev_win_avg = node_array[-1][1]
+                            else: # if there isn't any previous value
+                                prev_win_avg = 0.0
+                            # save the previous average in the node_array
+                            node_array.append((window_t, prev_win_avg))
+
+                        # increment the current window's time
+                        window_t += win_period
+                        # empty the win_data buffer
+                        win_data = []
 
             # sort the array
             node_array = sorted(node_array, key=lambda x: x[0] )
@@ -105,3 +104,54 @@ class UserStudyPlotter(cdp.CBLA_DataPlotter):
             active_arrays[node_name] = np.asarray(node_array)
 
         return active_arrays
+
+    def write_node_activation_array_to_excel(self, file_name=None, save_to_dir=None):
+
+        book = xlwt.Workbook(encoding="utf-8")
+
+        sheet = book.add_sheet("Node Activation", cell_overwrite_ok=True)
+
+        cur_row_id = 2
+        cur_col_id = 1
+
+        # sort the node_active_array
+        self.sorted_array_list = []
+        for node_name, node_data in self.node_active_array.items():
+            self.sorted_array_list.append((node_name, node_data))
+        self.sorted_array_list = sorted(self.sorted_array_list, key=lambda x: x[0])
+
+        sheet.write(1, 0, 'time (s)')
+        for node_name, node_data in self.sorted_array_list:
+
+            sheet.write(0, cur_col_id, node_name)
+            sheet.write(1, cur_col_id, 'output level')
+
+            for t, output in node_data:
+
+                sheet.write(cur_row_id, 0, t)
+                sheet.write(cur_row_id, cur_col_id, output)
+
+                cur_row_id += 1
+
+            cur_row_id = 2
+            cur_col_id += 1
+
+        # remember current directory
+        cur_dir = os.getcwd()
+
+        # if the directory is specified
+        if isinstance(save_to_dir, str):
+            # If the directory does not exist, create it
+            if not os.path.exists(save_to_dir):
+                os.makedirs(save_to_dir)
+
+            os.chdir(save_to_dir)
+
+        # if a file name is specified
+        if isinstance(file_name, str):
+            book.save(file_name)
+        else:
+            book.save("study_data.xls")
+
+        # go back to the "current directory"
+        os.chdir(cur_dir)
