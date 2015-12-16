c4a6b04130b445e8116205b0dbf6667cf29c1673
tuzzer <matthew.tk.chan@gmail.com>
2015-12-16 16:08:04 -0500

    change teensy interface to allow the print_to-term as a parameter set to kill itself when not hearing back from teensy devices

--- Software/complex_behaviours/cbla/cbla_user_study_analysis.py
+++ Software/complex_behaviours/cbla/cbla_user_study_analysis.py
@@ -7,14 +7,14 @@ from cbla_generic_node import *
 
 if __name__ == '__main__':
 
-    for i in range(1, 9):
+    for i in range(9, 11):
         log_dir = os.path.join(os.getcwd(), 'cbla_log', 'study_%d' % i)
 
         plotter = UserStudyPlotter(log_dir=log_dir, study_number=i, session_num=2,
                                    packet_types=(CBLA_Base_Node.cbla_data_type_key, CBLA_Base_Node.prescripted_data_type_key),
                                    info_types=(CBLA_Base_Node.cbla_state_type_key, CBLA_Base_Node.cbla_label_name_key,))
 
-        win_periods = (0.5, 1.0, 2.0, 5.0, 10.0, 20.0)
+        win_periods = (30.0, 60.0, 120.0, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0)
         for win_period in win_periods:
 
             plotter.node_active_array = plotter.compute_node_activation_array(win_period=win_period)
