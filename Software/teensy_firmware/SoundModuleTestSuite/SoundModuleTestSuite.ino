091aacfdb0cdac2382ac8ff5d292fe3e18e6ac15
David Kadish <david.kadish@gmail.com>
2016-01-14 17:42:15 -0500

    Started to add functionality to a test suite.

---
+++ Software/teensy_firmware/SoundModuleTestSuite/SoundModuleTestSuite.ino
@@ -0,0 +1,30 @@
+/* Test Suite for the Sound Modules in use in Series 3
+ * 
+ * Two Modes
+ * Automatic test > uses the sensors that it has on board
+ *                  to determine operation
+ * Manual test > runs through a test and awaits user feedback
+ */
+
+#define TWELVE_VOLT_LEDS 0 // Set 0 for 5V LEDs and 1 for 12V LEDS
+
+// Function prototypes
+void test_speaker(int speaker_id);
+int test_microphone(int microphone_id);
+void test_led(int led_id);
+int test_proximity(int proximity_id);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void test_speaker(int speaker_id){
+  
+}
+
